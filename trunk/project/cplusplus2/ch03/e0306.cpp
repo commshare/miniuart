af43c98f2cb65eb3c0e1a86e5efc134c81ab5deb
nie950@gmail.com <nie950@gmail.com@2650accd-41da-2f9a-4648-d3c9d4c3d8dd>
2013-07-15 09:39:31 +0000

    ch03 complete

---
+++ trunk/project/cplusplus2/ch03/e0306.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum[5]={0};
+	int iArray[]={1,4,2,7,13,32,21,48,16,30};
+	int size=sizeof(iArray)/sizeof(iArray[0]);
+	int *iPtr=iArray;
+	
+	for (int n=0;n<size;n++) {
+		sum[3] += iPtr[n];
+	}
+ 
+	for (int n=0;n<size;n++) {
+		sum[2] += *(iPtr+n);
+	} 
+	
+	for (int n=0;n<size;n++) {
+		sum[1] += *iPtr++;
+	} 
+	
+	for (int n=0;n<size;n++) {
+		sum[0] += iArray[n];
+	} 
+	
+	
+	for (int n=0;n<size;n++) {
+		sum[4] += *(iArray+n);
+	} 
+	
+	for (int n=0;n<5;n++) {
+		cout<<sum[n]<<endl;
+	} 
+	
+}
